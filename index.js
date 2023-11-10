2b4431d0b64d54fc8d4d1387b2c84ef1c2ae46d7
Andi-gech <andi.fab23@gmail.com>
2023-11-10 15:04:28 +0300

    ff

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+var cors = require("cors");
+const app = express();
+app.use(cors());
+const API_URL = "https://vidsrc.to/vapi/movie/new/2";
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+});
+
+app.get("/details/:id", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?i=${req.params.id}&apikey=137680e0`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ type: "error", message: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
